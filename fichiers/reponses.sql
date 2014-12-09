1b2c7412b600089a890a601549f5fc0958c5222e
TheMaster57 <maxime.blaise12@gmail.com>
2014-12-09 11:30:48 +0100

    Apport de correctifs pour la création des tables

--- fichiers/reponses.sql
+++ fichiers/reponses.sql
@@ -6,47 +6,42 @@ PRAGMA foreign_keys = ON;
 -- Question 1
 
 CREATE TABLE Boutique(
-	idB NUMBER(4),
+	idB INTEGER PRIMARY KEY AUTOINCREMENT,
 	nomBoutique VARCHAR2(30),
 	mail VARCHAR2(50),
 	numero NUMBER(4),
 	rue VARCHAR2(50),
 	ville VARCHAR2(30),
-	CP NUMBER(5)
+	CP NUMBER(5),
 	idGerant NUMBER(4),
-        PRIMARY KEY(idB),
 	FOREIGN KEY(idGerant) REFERENCES Personne(idP)
 );
 
 CREATE TABLE Personne(
-	idP NUMBER(4),
+	idP INTEGER PRIMARY KEY AUTOINCREMENT,
 	nom VARCHAR2(30),
 	prenom VARCHAR2(30),
 	mail VARCHAR2(50),
 	login VARCHAR2(30),
-	motDePasse VARCHAR2(30),
-	PRIMARY KEY(idP)
+	motDePasse VARCHAR2(30)
 );
 
 CREATE TABLE Marque(
-        idM NUMBER(4),
-        nomMarque VARCHAR(30),
-        PRIMARY KEY(idM)
+    idM INTEGER PRIMARY KEY AUTOINCREMENT,
+    nomMarque VARCHAR(30)
 );
 
 CREATE TABLE Produit(
-        idProd NUMBER(4),
+        idProd INTEGER PRIMARY KEY AUTOINCREMENT,
         libelle VARCHAR2(30),
         idMarque VARCHAR2(30),
         description VARCHAR2(30),
-        PRIMARY KEY(idProd),
         FOREIGN KEY(idMarque) REFERENCES Marque(idM)
 );
 
 CREATE TABLE PrixProduit(
         idProduit NUMBER(4),
         idBoutique NUMBER(4),
-        PRIMARY KEY(idProduit, idBoutique),
         FOREIGN KEY(idProduit) REFERENCES Produit(idP),
         FOREIGN KEY(idBoutique) REFERENCES Boutique(idB)
 );
