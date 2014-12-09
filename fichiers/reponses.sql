1a015099347373d14609d3cae3c18396c786fe0d
TheMaster57 <maxime.blaise12@gmail.com>
2014-12-09 10:52:49 +0100

    Premier commit du projet Netbeans

---
+++ fichiers/reponses.sql
@@ -0,0 +1,26 @@
+-- Maxime BLAISE
+-- Antoine NOSAL
+
+PRAGMA foreign_keys = ON;
+
+-- Question 1
+
+create table Personne(
+	idP NUMBER(4),
+	nom VARCHAR2(30),
+	prenom VARCHAR2(30),
+	mail VARCHAR2(50),
+	login VARCHAR2(30),
+	motDePasse VARCHAR2(30),
+	primary key(idP)
+);
+
+create table Boutique(
+	idB NUMBER(4),
+	nomBoutique VARCHAR2(30),
+	mail VARCHAR2(50),
+	numero NUMBER(4),
+	rue VARCHAR2(50),
+	ville VARCHAR2(30),
+	CP NUMBER(5)
+);
