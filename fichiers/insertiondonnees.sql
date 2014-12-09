29be5fbe56fdb3ff82010114fbf06b233ef1807e
TheMaster57 <maxime.blaise12@gmail.com>
2014-12-09 11:15:24 +0100

    Modification du fichier d'insertion
    .

--- fichiers/insertiondonnees.sql
+++ fichiers/insertiondonnees.sql
@@ -1,7 +1,20 @@
--- Insertion de tuples dans la table personne
+-- Insertion de tuples dans la table Personne
 INSERT INTO Personne(nom, prenom, mail, login, motDePasse) VALUES("Blaise", "Maxime", "maxime.blaise1@etu.univ-lorraine.fr", "blaise33u", "monmotdepasse");
 INSERT INTO Personne(nom, prenom, mail, login, motDePasse) VALUES("Nosal", "Antoine", "antoine.nosal@etu.univ-lorraine.fr", "nosal4u", "password");
 
--- Insertion de tuples dans la table boutique
+-- Insertion de tuples dans la table Boutique
 INSERT INTO Boutique(nomBoutique, mail, numero, rue, ville, CP, idGerant) VALUES("YiruMax", "maxime.blaise@yirumax.com", 28, "Aristide Briand", "Laxou", 54520, 1);
 INSERT INTO Boutique(nomBoutique, mail, numero, rue, ville, CP, idGerant) VALUES("TotoBoutique", "antoine.nosal@totoboutique.com", 50, "Aristide Briand", "Nancy", 54000, 2);
+
+-- Insertion de tuples dans la table Marque
+INSERT INTO Marque(nomMarque) VALUES("Adidas");
+INSERT INTO Marque(nomMarque) VALUES("Nike");
+INSERT INTO Marque(nomMarque) VALUES("Nokia");
+
+-- Insertion de tuples dans la table Produit
+INSERT INTO Produit(libelle, idMarque, description) VALUES("", 1, "");
+INSERT INTO Produit(libelle, idMarque, description) VALUES("", 1, "");
+INSERT INTO Produit(libelle, idMarque, description) VALUES("", 2, "");
+INSERT INTO Produit(libelle, idMarque, description) VALUES("", 2, "");
+INSERT INTO Produit(libelle, idMarque, description) VALUES("", 3, "");
+INSERT INTO Produit(libelle, idMarque, description) VALUES("", 3, "");
