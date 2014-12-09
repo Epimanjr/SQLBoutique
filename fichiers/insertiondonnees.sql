cc8c648fe2420ad1b1c6f410e147f620d0c9b614
TheMaster57 <maxime.blaise12@gmail.com>
2014-12-09 11:11:41 +0100

    Ajout d'un fichier d'insertion. (.sql)
    Pour répondre à la question 2.

---
+++ fichiers/insertiondonnees.sql
@@ -0,0 +1,7 @@
+-- Insertion de tuples dans la table personne
+INSERT INTO Personne(nom, prenom, mail, login, motDePasse) VALUES("Blaise", "Maxime", "maxime.blaise1@etu.univ-lorraine.fr", "blaise33u", "monmotdepasse");
+INSERT INTO Personne(nom, prenom, mail, login, motDePasse) VALUES("Nosal", "Antoine", "antoine.nosal@etu.univ-lorraine.fr", "nosal4u", "password");
+
+-- Insertion de tuples dans la table boutique
+INSERT INTO Boutique(nomBoutique, mail, numero, rue, ville, CP, idGerant) VALUES("YiruMax", "maxime.blaise@yirumax.com", 28, "Aristide Briand", "Laxou", 54520, 1);
+INSERT INTO Boutique(nomBoutique, mail, numero, rue, ville, CP, idGerant) VALUES("TotoBoutique", "antoine.nosal@totoboutique.com", 50, "Aristide Briand", "Nancy", 54000, 2);
